91781db595a8a67ad9652a74623f8b2bc4295cb8
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-06-22 10:01:00 -0400

    Fix: Rutas en main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -16,6 +15,7 @@ import Licencia from './views/Licencia.tsx';
 import { Exito } from './views/Exito.tsx';
 import Detalle_Adopcion from './views/Detalle_Adopcion.tsx';
 import DetalleReporte from './views/DetalleReporte.tsx';
+import AdoptionForm from './views/Adopciones.tsx';
 
 
 
@@ -31,6 +31,7 @@ const router = createBrowserRouter(
 			<Route path={APP_ROUTES.TERMS} element={<Terminos />} />
 			<Route path={APP_ROUTES.LINCENCE} element={<Licencia />} />
 			<Route path={APP_ROUTES.DETALLE_REPORTE_PARAMS} element={<DetalleReporte />} />
+      <Route path={APP_ROUTES.ADOPCIONES} element={<AdoptionForm />} />
 			<Route path="*" element={<p className='text-center fs-1 my-auto'>404 Path not resolved</p>} />
 		</Route>
 	)
@@ -45,45 +46,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	</React.StrictMode>,
 
 )
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import { APP_ROUTES } from "./helper/utility.tsx";
-import Home from "./views/Home.tsx";
-import Publicar from "./views/Publicar.tsx";
-import Buscar from "./views/Buscar.tsx";
-import Colaborar from "./views/Colaborar.tsx";
-import AdoptionForm from "./views/Adopciones.tsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="" element={<App />}>
-      <Route path={APP_ROUTES.HOME} element={<Home />} />
-      <Route path={APP_ROUTES.PUBLICAR_MASCOTA} element={<Publicar />} />
-      <Route path={APP_ROUTES.BUSCAR_MASCOTA} element={<Buscar />} />
-      <Route path={APP_ROUTES.COLABORAR} element={<Colaborar />} />
-      <Route path={APP_ROUTES.ADOPCIONES} element={<AdoptionForm />} />
-      <Route
-        path="*"
-        element={
-          <p className="text-center fs-1 my-auto">404 Path not resolved</p>
-        }
-      />
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
->>>>>>> AdoptionFormBranch
