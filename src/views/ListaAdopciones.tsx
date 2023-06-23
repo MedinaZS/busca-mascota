5c2b558152f5be653ec51e20a77b61d7387cef03
Agustin Ramirez <agusramirez2001@gmail.com>
2023-06-23 16:01:14 -0400

    Update: Cantidad de cards por fila en la lista

--- src/views/ListaAdopciones.tsx
+++ src/views/ListaAdopciones.tsx
@@ -60,7 +60,7 @@ const ListaAdopciones = () => {
 						<Link className='btn bg-blue-subtle rounded-pill fs-5' to={APP_ROUTES.PUBLICAR_ADOPCION}><i className='bi bi-plus'></i>Publicar adopcion </Link>
 					</div>
 
-					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4'>
+					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 '>
 						{listaAdopciones.map((adopcion: ResultAdopciones, index: number) => (
 							<Card key={index} adopcion={adopcion}></Card>
 						))}
